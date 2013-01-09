cf8789882ea05a86864af5caffa30db21781a89f
s.okada <kokuzen@gmail.com>
2013-01-10 01:07:55 +0900

    FieldTransfer.h is added.

---
+++ FieldTransfer.cpp
@@ -0,0 +1,13 @@
+#ifdef FIELDTRANSFER_UNIT_TEST
+#include "FieldTransfer.h"
+#include <iostream>
+
+using namespace field_transfer;
+
+int main(int argc, char* argv[])
+{
+
+    return 0;
+}
+
+#endif
---
+++ FieldTransfer.h
@@ -0,0 +1,14 @@
+#pragma once
+//FieldTransfer:20130110
+#include <iostream>
+#include "Formula.h"
+#include "Number.h"
+#include "Output.h"
+#include "Matrix.h"
+#include "Vector.h"
+#include "VectorList.h"
+#include "VectorListPair.h"
+namespace field_transfer
+{
+}
+
