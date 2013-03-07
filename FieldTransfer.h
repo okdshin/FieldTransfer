01b99cebb984cdf011c2b19bda21905edbc2c7b4
s.okada <kokuzen@gmail.com>
2013-03-07 09:33:16 +0900

    fixed.

--- FieldTransfer.h
+++ FieldTransfer.h
@@ -1,6 +1,14 @@
 #pragma once
 //FieldTransfer:20121220
 #include <iostream>
+#include "Formula.h"
+#include "Index.h"
+#include "Matrix.h"
+#include "Number.h"
+#include "Output.h"
+#include "Vector.h"
+#include "VectorList.h"
+#include "VectorListPair.h"
 
 namespace field_transfer
 {
